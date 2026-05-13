7d59f284ed4e8155b3cd1d486249a29fbce3bc4e
rosendo <rosendc30@gmail.com>
2026-05-12 21:57:24 -0300

    import icons e criação do componente botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "./components/button";
+import { Button } from "./components/Button";
 
 export function App() {
 
   return (
-    <div>
-      <h1 className="text-3xl">Hello, Vite + React!</h1>
+    <div className="bg-[var(--gray-600)] text-[var(--gray-200)] max-w-[24.375rem] mx-auto border border-gray-300 p-4 rounded-lg">
+      <h1 className="text-3xl font-bold">Hello, Vite + React!</h1>
       <Button />
     </div>
   )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@
 export function Button() {
 
     return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-[var(--gray-200)] hover:bg-[var(--gray-100)] text-[var(--gray-600)] font-bold py-2 px-4 rounded">
             Button
         </button>
     )
