ad1db0bbde09fd48f502a4cf07d528220152361f
rosendo <rosendc30@gmail.com>
2026-05-15 21:37:17 -0300

    config tailwind e assets

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,13 @@ import IconX from "./assets/icon/x.svg?react";
 export function App() {
 
   return (
-    <div className="flex flex-col bg-[var(--gray-600)] text-[var(--gray-200)] max-w-[24.375rem] mx-auto border border-gray-300 p-4">
+    <div className="flex flex-col max-w-md mx-auto border border-gray-300 p-4">
+
+      <h1 className="text-blue-dark text-3xl font-bold">Tailwind CSS - Button Icon</h1>
 
       <IconHamburger className="fill-amber-600" />
 
       <ButtonIcon icon={<IconX />} />
-
 
     </div>
   )
